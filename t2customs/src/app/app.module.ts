8b4e7b3204e9aa6cf2c57d54e71d8cd2a813f6f8
Eric Newman <googlnewman@gmail.com>
2021-04-27 00:56:18 -0500

    added a bunch of empty components that represent the imagined structure of the project - currently code sitting inside home component and needs to be broken to separate components (or would like to do so). Working on figuring out the navbar at the moment - added background image for it was working on sizing for the image.

--- t2customs/src/app/app.module.ts
+++ t2customs/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    NavbarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
